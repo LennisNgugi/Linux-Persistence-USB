b62abc384398d317acbff1950e1fe09384bb9a3c
734415 <42470457+LennisNgugi@users.noreply.github.com>
2020-10-12 17:13:03 +0100

    Update Linux_Persistence.sh

--- Linux_Persistence.sh
+++ Linux_Persistence.sh
@@ -1,10 +1,11 @@
+#!/bin/bash 
 #Format USB as FAT32 
 #Select "Live Persistence"
 #Open terminal
 #Type the following commands
-sudo su
-fdisk -l
-mkdir -p /mnt/rename_usb_persistence
-mount /dev/sdb2 /mnt/rename_usb_persistence
-echo "/ union" > /mnt/rename_usb_persistence/persistence.conf
-umount /dev/sdb2 && reboot
+sudo su;
+fdisk -l;
+mkdir -p /mnt/rename_usb_persistence;
+mount /dev/sdb2 /mnt/rename_usb_persistence;
+echo "/ union" > /mnt/rename_usb_persistence/persistence.conf;
+umount /dev/sdb2 && reboot;
